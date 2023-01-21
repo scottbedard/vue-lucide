5ee65410448619eed5530d46fcdfe5d9d041cca0
Scott Bedard <scottbedard90@gmail.com>
2023-01-21 01:01:44 -0600

    sandbox scaffold

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="flex items-center">
-    Hello, I'm Mr. Meeseeks! Look at me! <ArrowRightIcon size="20" />
+  <div class="gap-6 grid">
+    <div class="gap-1 grid">
+      <h1 class="font-bold font-mono text-2xl">
+        <a href="https://github.com/scottbedard/vue-lucide">
+          @bedard/vue-lucide
+        </a>
+      </h1>
+
+      <div class="text-gray-800 tracking-wide">
+        Tree shakeable Vue components for <a href="https://lucide.dev/" target="_blank">Lucide icons</a>
+      </div>
+    </div>
+
+    <div class="border-t-2 border-gray-300" />
+
+    <div>
+      A proper sandbox is coming soon, give it time.
+    </div>
   </div>
 </template>
 
--- sandbox/tailwind.config.cjs
+++ sandbox/tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: '"Source Code Pro", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+        sans: 'Quicksand, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      },
+    },
   },
   plugins: [],
 }
