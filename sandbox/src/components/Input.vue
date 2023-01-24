f33be374d85209953eac2e97a1a04f11cae1bae8
Scott Bedard <scottbedard90@gmail.com>
2023-01-23 23:40:23 -0600

    fix ignored files

---
+++ sandbox/src/components/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <input
+    class="border border-gray-300 h-10 rounded-md px-4"
+    :value="modelValue"
+    @input="$emit('update:modelValue', ($event.target as any).value)" />
+</template>
+
+<script lang="ts" setup>
+defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+defineProps<{
+  modelValue: string
+}>()
+</script>
