98f4761ceb9ce2a8d5add61f27fef51f73f612fb
Scott Bedard <scottbedard90@gmail.com>
2023-01-24 00:44:43 -0600

    styling

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -13,22 +13,26 @@
     </Margin>
 
     <div
-      class="bg-gray-100 sticky py-6 top-0 md:gap-6 xl:gap-8"
+      class="sticky top-0"
       ref="searchContainer">
-      <Margin>
-        <Input
-          v-model="search"
-          autofocus
-          class="text-sm w-full"
-          placeholder="Search icons (Press &quot;/&quot; to focus)" />
-      </Margin>
+      <div class="bg-gray-100 pt-6">
+        <Margin>
+          <input
+            v-model="search"
+            autofocus
+            class="border border-gray-200 h-12 rounded-md px-4 shadow-md w-full"
+            placeholder="Search icons (press any key to focus)" />
+        </Margin>
+      </div>
+
+      <div class="bg-gradient-to-b from-gray-100 to-transparent h-6" />
     </div>
 
     <Margin>
       <div class="gap-3 grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10 sm:gap-4 md:gap-6">
         <div
           v-for="[name, icon] in Object.entries(VueLucide)"
-          class="aspect-square bg-white flex flex-col gap-4 items-center justify-center overflow-hidden text-ellipsis p-4 rounded shadow-md"
+          class="aspect-square bg-white flex flex-col gap-4 items-center justify-center overflow-hidden text-ellipsis p-4 rounded-lg shadow-md"
           :key="name">
           <div>
             <Component
@@ -37,7 +41,7 @@
           </div>
 
           <div
-            v-text="name"
+            v-text="name.substring(0, name.length - 4)"
             class="overflow-hidden text-center text-ellipsis text-xs tracking-wide w-full md:text-sm" />
         </div>
       </div>
@@ -46,24 +50,18 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import { useEventListener } from '@vueuse/core'
 import * as VueLucide from '@bedard/vue-lucide'
-import Icon from '@/components/Icon.vue'
-import Input from '@/components/Input.vue'
 import Margin from '@/components/Margin.vue'
 
 const search = ref('')
 
 const searchContainer = ref<HTMLElement>()
 
-const entries = computed(() => Object.entries(VueLucide))
-
 useEventListener('keypress', e => {
-  if (e.key === '/') {
-    e.preventDefault()
+  e.preventDefault()
 
-    searchContainer.value?.querySelector('input')?.focus()
-  }
+  searchContainer.value?.querySelector('input')?.focus()
 })
 </script>
